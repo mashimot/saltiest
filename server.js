06e99a64099f505b82bd782c3cbfa09bfc23d348
Daniel Hashimoto <linkhashimoto@hotmail.com>
2018-12-21 07:13:17 -0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
+// Serve static files
 app.use(express.static(__dirname + '/dist/saltiest'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/saltiest/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/saltiest/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
