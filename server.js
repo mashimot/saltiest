b1123f2af70bfdb8db1de0751a4cbec1677091a9
Daniel Hashimoto <linkhashimoto@hotmail.com>
2018-12-21 06:47:31 -0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/saltiest/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(8080);
