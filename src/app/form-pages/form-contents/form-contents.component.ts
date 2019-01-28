f107082f309572bcbf335cadd592a1ed256eb659
Daniel Hashimoto <dhashimoto@unisa.br>
2019-01-28 13:37:10 -0200

    Copy html is working now

--- src/app/form-pages/form-contents/form-contents.component.ts
+++ src/app/form-pages/form-contents/form-contents.component.ts
@@ -31,7 +31,30 @@ export class FormContentsComponent implements OnInit {
     }
 
     copyHtml(content): void{
+        this.renderHtmlService.setParams(content);
+        let code = this.renderHtmlService.get().html;
+        this.copyToClipboard(code);
+    }
 
+    copyToClipboard(text) {
+        if ((<any>window).clipboardData && (<any>window).clipboardData.setData) {
+            // IE specific code path to prevent textarea being shown while dialog is visible.
+            return (<any>window).clipboardData.setData("Text", text);
+        } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+            var textarea = document.createElement("textarea");
+            textarea.textContent = text;
+            textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+            document.body.appendChild(textarea);
+            textarea.select();
+            try {
+                return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+            } catch (ex) {
+                console.warn("Copy to clipboard failed.", ex);
+                return false;
+            } finally {
+                document.body.removeChild(textarea);
+            }
+        }
     }
 
     sendDataToModal(content): void {
